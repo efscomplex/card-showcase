51e1425c42d94673ad24fdaa605e5520c3aba493
edgar <edgar@Air-de-edgar.home>
2021-05-26 13:05:42 +0200

    netlify env vars

--- src/www/config.ts
+++ src/www/config.ts
@@ -1,2 +1,2 @@
-export const TIENDEO_URL = process.env.TIENDEO_URL as string
-export const TIENDEO_TOKEN_NAME = process.env.TIENDEO_TOKEN_NAME as string
+export const TIENDEO_URL = 'https://tiendeo-frontend-cards-api.herokuapp.com/' //process.env.TIENDEO_URL as string
+export const TIENDEO_TOKEN_NAME = 'tiendeo_token' //process.env.TIENDEO_TOKEN_NAME as string
